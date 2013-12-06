63408d5b77204e177607ee8f92c48dc8f1335812
Rolf v.d. Forst <rolf.v.d.forst@googlemail.com>
2013-12-06 12:05:28 +0100

    added translation module

---
+++ modules/translation/translation.php
@@ -0,0 +1,318 @@
+<?php
+class Workflow_Translation extends Workflow_Module {
+
+    const translate_from_lang_post_meta = '_translate_from_lang_post_meta';
+    
+    const translate_to_lang_post_meta = '_translate_to_lang_post_meta';
+ 
+    public $module;
+    
+    public function __construct() {
+		global $cms_workflow;
+		
+		$this->module_url = $this->get_module_url( __FILE__ );
+        
+		$args = array(
+			'title' => __( 'Übersetzung', CMS_WORKFLOW_TEXTDOMAIN ),
+			'description' => __( 'Import und Export von Xiff-Dateien.', CMS_WORKFLOW_TEXTDOMAIN ),
+			'module_url' => $this->module_url,
+			'slug' => 'translation',
+			'default_options' => array(
+				'post_types' => array(
+					'post' => true,
+					'page' => true
+				),
+			),
+			'configure_callback' => 'print_configure_view',
+			'settings_help_tab' => array(
+				'id' => 'workflow-translation-overview',
+				'title' => __('Übersicht', CMS_WORKFLOW_TEXTDOMAIN),
+				'content' => __('<p></p>', CMS_WORKFLOW_TEXTDOMAIN),
+				),
+			'settings_help_sidebar' => __( '<p><strong>Für mehr Information:</strong></p><p><a href="http://blogs.fau.de/cms">Dokumentation</a></p><p><a href="http://blogs.fau.de/webworking">RRZE-Webworking</a></p><p><a href="https://github.com/RRZE-Webteam">RRZE-Webteam in Github</a></p>', CMS_WORKFLOW_TEXTDOMAIN ),
+		);
+        
+		$this->module = $cms_workflow->register_module( 'translation', $args );
+	}
+	
+	public function init() {
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
+        
+        add_filter( 'upload_mimes', array( $this, 'xliff_mime_type') );
+        
+        add_action('post_edit_form_tag', array( $this, 'update_edit_form'));
+        
+        add_action('add_meta_boxes', array( $this, 'translate_meta_box'));
+        
+        add_action('save_post', array( $this, 'save_translate_meta_data'));
+        
+        add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+	}
+
+    public static function update_post_content( $post_id, $post ) {
+        if ( ! wp_is_post_revision( $post_id ) ) {
+            remove_action('save_post', array( $this, 'update_post_content' ));
+
+            $args = array(
+                'ID' => $post_id,
+                'post_content' => $post->post_content,
+            );
+
+            wp_update_post($args);
+
+            add_action( 'save_post', array( $this, 'update_post_content' ));
+        }
+                
+    }
+    
+	public function register_settings() {
+			add_settings_section( $this->module->workflow_options_name . '_general', false, '__return_false', $this->module->workflow_options_name );
+			add_settings_field( 'post_types', __( 'Freigabe', CMS_WORKFLOW_TEXTDOMAIN ), array( $this, 'settings_post_types_option' ), $this->module->workflow_options_name, $this->module->workflow_options_name . '_general' );
+	}
+	
+	public function settings_post_types_option() {
+		global $cms_workflow;
+		$cms_workflow->settings->custom_post_type_option( $this->module );	
+	}
+
+	public function settings_validate( $new_options ) {
+		
+		if ( !isset( $new_options['post_types'] ) )
+			$new_options['post_types'] = array();
+        
+		$new_options['post_types'] = $this->clean_post_type_options( $new_options['post_types'], $this->module->post_type_support );
+		
+		return $new_options;
+
+	}	
+
+	public function print_configure_view() {
+		?>
+		<form class="basic-settings" action="<?php echo esc_url( menu_page_url( $this->module->settings_slug, false ) ); ?>" method="post">
+			<?php settings_fields( $this->module->workflow_options_name ); ?>
+			<?php do_settings_sections( $this->module->workflow_options_name ); ?>
+			<?php
+				echo '<input id="cms_workflow_module_name" name="cms_workflow_module_name" type="hidden" value="' . esc_attr( $this->module->name ) . '" />';				
+			?>
+			<p class="submit"><?php submit_button( null, 'primary', 'submit', false ); ?></p>
+		</form>
+		<?php
+	}	
+
+    public function update_edit_form() {  
+        echo ' enctype="multipart/form-data"';  
+    }
+
+    public function xliff_mime_type( $mime_types ) {
+        //XLIFF: XML Localisation Interchange File Format
+        $mime_types['xliff'] = 'application/octet-stream';
+        $mime_types['xlf'] = 'application/octet-stream';
+        return $mime_types;
+    }
+
+    public function translate_meta_box() {
+        global $post;
+
+        if($this->module_activated( 'post_versioning' ) && in_array( $post->post_status, array('publish', 'future', 'private') ))
+            return;
+        
+        add_meta_box(
+            'translate',
+            __('Übersetzung', CMS_WORKFLOW_TEXTDOMAIN),
+            array( $this, 'translate_meta_box_fields'),
+            'page',
+            'normal'
+        );
+        
+    }
+
+    public function translate_meta_box_fields() {
+        global $post;
+        
+        $post_id = $post->ID;
+        
+        $site_lang = get_option('WPLANG') ? get_option('WPLANG') : 'en_EN';
+        $translate_from_lang = 0;
+        
+        if($this->module_activated( 'post_versioning' )) {
+            
+                $remote_post_meta = get_post_meta( $post_id, Workflow_Post_Versioning::version_remote_post_meta, true );
+
+                if(isset($remote_post_meta['post_id']) && isset($remote_post_meta['blog_id'])) {
+                    if(switch_to_blog( $remote_post_meta['blog_id'] )) {
+
+                        if(!is_null(get_post($remote_post_meta['post_id']))) {
+                            $translate_from_lang = get_option('WPLANG') ? get_option('WPLANG') : 'en_EN';
+                        }                
+
+                        restore_current_blog();
+
+                    }
+                }
+            
+        }
+        
+        wp_nonce_field(plugin_basename(__FILE__), 'translate_fields_nonce');
+        
+        if(!empty($translate_from_lang)) {
+            
+            $translate_to_lang = $site_lang;
+            
+            update_post_meta($post_id, self::translate_from_lang_post_meta, $translate_from_lang);
+            update_post_meta($post_id, self::translate_to_lang_post_meta, $translate_to_lang);
+            
+            $html = '<input type="hidden" name="translate_from_lang" value="' . $translate_from_lang . '" />';
+            $html .= '<input type="hidden" name="translate_to_lang" value="' . $translate_to_lang . '" />';
+            $html .= '<p>' . sprintf(__('Übersetzung von <i>%1$s</i> nach <i>%2$s</i>.', CMS_WORKFLOW_TEXTDOMAIN), $this->get_lang_name($translate_from_lang), $this->get_lang_name($translate_to_lang)) . '</p>';
+                   
+        } else {
+            
+            $translate_from_lang_post_meta = get_post_meta($post_id, self::translate_from_lang_post_meta, true);
+            $translate_from_lang = $translate_from_lang_post_meta != '' ? $translate_from_lang_post_meta : 0;
+            
+            $translate_to_lang_post_meta = get_post_meta($post_id, self::translate_to_lang_post_meta, true);
+            $translate_to_lang = $translate_to_lang_post_meta == '' ? $site_lang : $translate_to_lang_post_meta;
+            
+            $html = '<label>' . __('Aus der Sprache:', CMS_WORKFLOW_TEXTDOMAIN) . '&nbsp;';
+            $html .= '<select id="translate-from-lang" name="translate_from_lang">';
+            $html .= '<option value="0">' . __('Wählen', CMS_WORKFLOW_TEXTDOMAIN) . '</option>';
+
+            foreach($this->lang_codes() as $key => $value) {
+                $html .= sprintf('<option value="%1$s"' . selected( $translate_from_lang, $key, false ) . '>%2$s</option>', $key, $value);
+            }
+
+            $html .= '</select></label>&nbsp;';
+
+            $html .= '<label>' . __('In die Sprache:', CMS_WORKFLOW_TEXTDOMAIN) . '&nbsp;';
+            $html .= '<select id="translate-to-lang" name="translate_to_lang">';
+            $html .= '<option value="0">' . __('Wählen', CMS_WORKFLOW_TEXTDOMAIN) . '</option>';
+
+            foreach($this->lang_codes() as $key => $value) {
+                $html .= sprintf('<option value="%1$s"' . selected( $translate_to_lang, $key, false ) . '>%2$s</option>', $key, $value);
+            }
+
+            $html .= '</select></label>';
+            
+            $html .= '<p class="description">' . __('Bitte wählen Sie die Übersetzungssprachen aus', CMS_WORKFLOW_TEXTDOMAIN) . '</p>';
+        }
+        
+        $cap = $this->get_available_post_types($post->post_type)->cap;
+        
+        if($translate_from_lang && current_user_can($cap->edit_posts)) {
+            $download_link = $this->module_url . 'xliff-download.php';
+            $download_link = add_query_arg('xliff-attachment', $post_id, $download_link);
+            
+            $html .= sprintf('<p><a href="%1$s">%2$s</a> ', $download_link, __('XLIFF-Datei herunterladen', CMS_WORKFLOW_TEXTDOMAIN));
+
+            $html .= '<p class="label">' . __('XLIFF-Datei hochladen:', CMS_WORKFLOW_TEXTDOMAIN) . '&nbsp;';
+            $html .= '<input type="file" id="translate_xliff_attachment" name="translate_xliff_attachment" value="" size="25">'; 
+            $html .= '</p>';
+        }
+        
+        echo $html;
+
+    }
+    
+    public function save_translate_meta_data($post_id) {
+
+        if(!isset($_POST['translate_fields_nonce']) || !wp_verify_nonce($_POST['translate_fields_nonce'], plugin_basename(__FILE__))) {
+            return $post_id;
+        }
+
+        if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return $post_id;
+        }
+        
+        $cap = $this->get_available_post_types($post->post_type)->cap;
+        
+        if(!current_user_can($cap->edit_posts)) {
+            return $post_id;
+        }
+        
+        if(!$this->module_activated( 'post_versioning' ) || !in_array( $post->post_status, array('publish', 'future', 'private') )) {
+            if(isset($_POST['translate_from_lang']) && !empty($_POST['translate_from_lang']) && isset( $_POST['translate_to_lang']) && !empty($_POST['translate_to_lang'])) {
+                if($_POST['translate_from_lang'] != $_POST['translate_to_lang']) {
+                    update_post_meta( $post_id, self::translate_from_lang_post_meta, esc_attr( $_POST['translate_from_lang'] ) );
+                    update_post_meta( $post_id, self::translate_to_lang_post_meta, esc_attr( $_POST['translate_to_lang'] ) );
+                }
+            }
+        }
+        
+        if(get_post_meta($post_id, self::translate_from_lang_post_meta, true) != '' && get_post_meta($post_id, self::translate_to_lang_post_meta, true) != '') {
+            if(!empty($_FILES['translate_xliff_attachment']['name']) && !empty($_FILES['translate_xliff_attachment']['tmp_name'])) {
+                $supported_types = array('application/octet-stream');
+
+                $arr_file_type = wp_check_filetype(basename($_FILES['translate_xliff_attachment']['name']));
+
+                if(in_array($arr_file_type['type'], $supported_types)) {
+
+                    $error = $this->import_xliff($post_id, $_FILES['translate_xliff_attachment']);
+                    if ( is_wp_error($error) )
+                        $this->flash_admin_notice($error->get_error_message(), 'error');
+
+                } else {
+                    $this->flash_admin_notice(__('Der Dateityp, die Sie hochgeladen haben, ist nicht eine XLIFF.', CMS_WORKFLOW_TEXTDOMAIN), 'error');
+                }
+            }
+        }
+	
+    }
+
+	public function import_xliff($post_id, $file) {
+		
+        if ( wp_is_post_revision( $post_id ) )
+            return;
+        
+        remove_action('save_post', array( $this, 'save_translate_meta_data'));
+        
+        $blog_id = get_current_blog_id();
+		
+        $fh = fopen($file['tmp_name'], 'r');
+        
+        $data = fread($fh, $file['size']);
+        
+        fclose($fh);
+        clearstatcache();
+        
+        if ( ! function_exists('simplexml_load_string'))
+            return new WP_Error('xml_missing', __('Die "Simple XML"-Bibliothek fehlt.', CMS_WORKFLOW_TEXTDOMAIN));
+
+        $xml = simplexml_load_string($data);
+
+        if (!$xml)
+            return new WP_Error('not_xml_file', sprintf(__('Die Xliff-Datei (%s) konnte nicht gelesen werden.', CMS_WORKFLOW_TEXTDOMAIN), $name));
+
+        $file_attributes = $xml->file->attributes();
+        if (!$file_attributes || !isset($file_attributes['original']))
+            return new WP_Error('not_xml_file', sprintf(__('Die Xliff-Datei (%s) konnte nicht gelesen werden.', CMS_WORKFLOW_TEXTDOMAIN), $name));
+
+        $original = (string)$file_attributes['original'];
+
+        if ($original != md5(sprintf('%d - %d', $blog_id, $post_id)))
+            return new WP_Error('xliff_doesnt_match', __('Die hochgeladene Xliff-Datei ist nicht für dieses Dokument geeignet.', CMS_WORKFLOW_TEXTDOMAIN));
+
+        $post_array = array('ID' => $post_id);
+        
+        foreach ($xml->file->body->children() as $node) {
+            $attr = $node->attributes();
+            $type = (string)$attr['id'];
+            if($type == 'title') {
+                $post_array['post_title'] = (string)$node->target;                
+            } elseif($type == 'body') {
+                $target = (string)$node->target;
+                $post_array['post_content'] = str_replace('<br class="xliff-newline" />', PHP_EOL, $target);
+            } elseif($type == 'excerpt') {
+                $post_array['post_excerpt'] = (string)$node->target;
+            }
+            
+        }
+		
+        if(!wp_update_post( $post_array ))
+            return new WP_Error('post_update_error', __('Ein unbekannter Fehler ist aufgetreten. Das Dokument konnte nicht gespeichert werden.', CMS_WORKFLOW_TEXTDOMAIN));               
+
+	}
+        
+    public function admin_notices() {
+        $this->show_flash_admin_notices();
+    }
+}
---
+++ modules/translation/xliff-download.php
@@ -0,0 +1,131 @@
+<?php
+if(empty($_GET['xliff-attachment']))
+    exit;
+
+$post_id = (int) $_GET['xliff-attachment'];
+
+if(empty($post_id))
+    exit;
+
+define( 'SHORTINIT', true );
+
+require_once( dirname( dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) ) . '/wp-load.php' );
+
+wp_not_installed();
+
+require( ABSPATH . WPINC . '/formatting.php' );
+require( ABSPATH . WPINC . '/capabilities.php' );
+require( ABSPATH . WPINC . '/user.php' );
+require( ABSPATH . WPINC . '/meta.php' );
+require( ABSPATH . WPINC . '/post.php' );
+require( ABSPATH . WPINC . '/kses.php' );
+require_once( ABSPATH . WPINC . '/general-template.php' );
+
+wp_plugin_directory_constants( );
+
+if ( is_multisite() )
+	ms_cookie_constants(  );
+
+wp_cookie_constants( );
+
+require( ABSPATH . WPINC . '/pluggable.php' );
+
+if(!is_user_logged_in())
+    exit;
+
+xliff_attachment($post_id);
+
+function xliff_attachment($post_id) {		      
+    $xliff_file = get_xliff_file($post_id);
+    if(!$xliff_file)
+        exit;
+    
+    $sitename = sanitize_key( get_bloginfo( 'name' ) );
+    if ( ! empty($sitename) ) 
+        $sitename .= '-';
+
+    $filename = sprintf('%1$s%2$s-%3$s.xliff', $sitename, $post_id, date( 'Y-m-d' ));
+
+    header( 'Content-Description: File Transfer' );
+    header( 'Content-Disposition: attachment; filename=' . $filename );
+    header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
+
+    echo $xliff_file;
+    exit;		
+}
+
+function get_xliff_file($post_id) {
+    $blog_id = get_current_blog_id();
+    $post = get_post($post_id);
+
+    if(is_null($post))
+        return false;
+    
+    $source_language_code = get_post_meta($post_id, '_translate_from_lang_post_meta', true);   
+    if($source_language_code == '')
+        return false;
+
+    $source_language_code = explode('_', $source_language_code);
+    $source_language_code = $source_language_code[0];
+    
+    $language_code = get_post_meta($post_id, '_translate_to_lang_post_meta', true);
+    if($language_code == '')
+        return false;
+    
+    $language_code = explode('_', $language_code);
+    $language_code = $language_code[0];
+    
+    $translation = (object) array(
+        'original' => md5(sprintf('%d - %d', $blog_id, $post_id)), 
+        'source_language_code' => $source_language_code,
+        'language_code' => $language_code,
+        'elements' => array(
+            (object) array(
+                'field_type' => 'title',
+                'field_data' => $post->post_title,
+                'field_data_translated' => $post->post_title,                   
+            ),
+            (object) array(
+                'field_type' => 'body',
+                'field_data' => $post->post_content,
+                'field_data_translated' => $post->post_content,                   
+            ), 
+            (object) array(
+                'field_type' => 'excerpt',
+                'field_data' => $post->post_excerpt,
+                'field_data_translated' => $post->post_excerpt,                   
+            ),                                
+        ),
+    );
+
+    $xliff_file  = '<?xml version="1.0" encoding="utf-8" standalone="no"?>' . PHP_EOL;
+    $xliff_file .= '<!DOCTYPE xliff PUBLIC "-//XLIFF//DTD XLIFF//EN" "http://www.oasis-open.org/committees/xliff/documents/xliff.dtd">' . PHP_EOL;
+    $xliff_file .= '<xliff version="1.0">' . PHP_EOL;
+    $xliff_file .= '   <file original="' . $translation->original . '" source-language="' . $translation->source_language_code . '" target-language="' . $translation->language_code . '" datatype="plaintext">' . PHP_EOL;
+    $xliff_file .= '      <header></header>' . PHP_EOL;
+    $xliff_file .= '      <body>' . PHP_EOL;
+
+    foreach ($translation->elements as $element) {				
+        $field_data = $element->field_data;
+        $field_data_translated = $element->field_data_translated;
+
+        if ($field_data != '') {
+            $field_data = str_replace(PHP_EOL, '<br class="xliff-newline" />', $field_data);
+            $field_data_translated = str_replace(PHP_EOL, '<br class="xliff-newline" />', $field_data_translated);
+
+            $xliff_file .= '         <trans-unit resname="' . $element->field_type. '" restype="String" datatype="text|html" id="' . $element->field_type. '">' . PHP_EOL;
+
+            $xliff_file .= '            <source><![CDATA[' . $field_data . ']]></source>' . PHP_EOL;
+
+            $xliff_file .= '            <target><![CDATA[' . $field_data_translated . ']]></target>' . PHP_EOL;
+
+            $xliff_file .= '         </trans-unit>' . PHP_EOL;
+        }
+    }
+
+    $xliff_file .= '      </body>' . PHP_EOL;
+    $xliff_file .= '   </file>' . PHP_EOL;
+    $xliff_file .= '</xliff>' . PHP_EOL;
+
+    return $xliff_file;
+}
