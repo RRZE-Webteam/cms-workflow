ae41d9e8f582e365339fabe900dac020394a1eff
rvdforst <rolf.v.d.forst@fau.de>
2017-04-03 15:29:29 +0200

    Update plugin version.

--- cms-workflow.php
+++ cms-workflow.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: CMS-Workflow
   Plugin URI: https://github.com/RRZE-Webteam/cms-workflow
-  Version: 1.12.3
+  Version: 1.12.4
   Description: Redaktioneller Workflow.
   Author: RRZE-Webteam
   Author URI: https://blogs.fau.de/webworking
@@ -33,7 +33,7 @@ register_deactivation_hook(__FILE__, array('CMS_Workflow', 'deactivation_hook'))
 
 class CMS_Workflow {
 
-    const version = '1.12.3'; // Plugin-Version
+    const version = '1.12.4'; // Plugin-Version
     const textdomain = 'cms-workflow';
     const php_version = '5.5'; // Minimal erforderliche PHP-Version
     const wp_version = '4.7'; // Minimal erforderliche WordPress-Version
