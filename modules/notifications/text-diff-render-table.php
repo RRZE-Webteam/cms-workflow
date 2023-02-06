05e56eaca6e391741b12ae61e266055d1622ebb1
rvdforst <rolf.v.d.forst@fau.de>
2023-02-06 13:54:38 +0100

    Update text-diff-render-table.php

--- modules/notifications/text-diff-render-table.php
+++ modules/notifications/text-diff-render-table.php
@@ -1,6 +1,7 @@
 <?php
 
-class Workflow_Text_Diff_Renderer_Table extends WP_Text_Diff_Renderer_Table {
-	var $_leading_context_lines  = 2;
-	var $_trailing_context_lines = 2;
+class Workflow_Text_Diff_Renderer_Table extends WP_Text_Diff_Renderer_Table
+{
+    var $_leading_context_lines  = 2;
+    var $_trailing_context_lines = 2;
 }
